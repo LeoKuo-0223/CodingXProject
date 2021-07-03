059e2411e22426688b36bfdb7d383c54d7214a0e
LeoKuo-0223 <leo900223@gmail.com>
2021-07-03 23:39:06 +0800

    Initial commit

--- android/screen/Home.js
+++ android/screen/Home.js
@@ -1,27 +1,83 @@
-import React from 'react';
-import {StyleSheet, View, Text, Button} from 'react-native'
+// import React from 'react';
+// import { StyleSheet, View, Text, Button } from 'react-native'
 
-export default function Home({navigation}){
-   
-
-    const pressHandler = () =>{
-        navigation.navigate('月曆模式')
-    }
+// export default function Home({ navigation }) {
 
 
-    return(
-        <View style={styles.container}>
-            <Text style={styles.text}>Home Screen</Text>
-            <View style={styles.button}>
-            <Button title = '月曆模式' onPress={pressHandler }
-            ></Button>
+//     const pressHandler1 = () => {
+//         navigation.navigate('月曆模式')
+//     }
+
+//     const pressHandler3 = () => {
+//         navigation.navigate('日曆模式')
+//     }
+
+
+//     return (
+//         <View style={styles.container}>
+//             <Text style={styles.text}>Home Screen</Text>
+//             <View style={styles.button}>
+//                 <Button title='月曆模式' onPress={pressHandler1}
+//                 ></Button>
+//             </View>
+//             <View style={styles.button}>
+//                 <Button title='日曆模式' onPress={pressHandler3}
+//                 ></Button>
+//             </View>
+//         </View>
+
+//     )
+// }
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: 'pink',
+//         paddingVertical: 32,
+//         padding: 16,
+//     },
+//     text: {
+//         textAlign: 'center'
+//     },
+//     button: {
+//         padding: 32,
+//     }
+// })
+
+
+
+
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native'
+import { withNavigation } from 'react-navigation';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+
+export default class Home extends React.Component{
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.text}>Home Screen</Text>
+                <View style={styles.button}>
+                    <Button title='月曆模式' 
+                    onPress={()=>this.props.navigation.navigate('月曆模式')}
+                    ></Button>
+                </View>
+                <View style={styles.button}>
+                    <Button title='日曆模式' 
+                    onPress={()=>this.props.navigation.navigate('日曆模式')}
+                    />
+                </View>
             </View>
-        </View>
-    )
+
+        )
+    }
 }
+
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
+        backgroundColor: 'pink',
         paddingVertical: 32,
         padding: 16,
     },
--- android/testNavigator/Homestack.js
+++ android/testNavigator/Homestack.js
@@ -1,8 +1,7 @@
 import {createStackNavigator} from 'react-navigation-stack';
 import {createAppContainer} from 'react-navigation';
 import Home from '../screen/Home';
-import YearMode from '../screen/MonthMode';
-import MonthMode from '../screen/YearMode';
+import MonthMode from '../screen/MonthMode';
 import DateMode from '../screen/DateMode';
 
 const AppNavigator = {
@@ -10,8 +9,11 @@ const AppNavigator = {
         screen: Home
     },
     月曆模式: {
-        screen: YearMode
-    }
+        screen: MonthMode
+    },
+    日曆模式: {
+        screen: DateMode
+    },
 }
 
 const Homestack = createStackNavigator(AppNavigator);
